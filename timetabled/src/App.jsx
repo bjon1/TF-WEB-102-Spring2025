240e1028ac5170ae027d5d3ca116aa8af34e9020
nebjamin <jonb1@newpaltz.edu>
2025-02-22 00:28:30 -0500

    Lab 1 Checkpoint 2

--- timetabled/src/App.jsx
+++ timetabled/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Calendar from './components/Calendar';
 
 const App = () => {
 
@@ -6,7 +7,7 @@ const App = () => {
     <div className="App">
       <h1>Itinerary for 7 Days in Chicago🏙</h1>
       <h2>Welcome to Chicago, Natalia! Check out this calendar to get to know the city and see all the sights during your stay.</h2>
-      
+      <Calendar />
     </div>
   )
 }
