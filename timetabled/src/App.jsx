9dc1f7a7e6c693c5e8060d37271f2c9529e39e2d
nebjamin <jonb1@newpaltz.edu>
2025-02-22 00:55:21 -0500

    Lab 1 Add Aesthetic Changes

--- timetabled/src/App.jsx
+++ timetabled/src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Itinerary for 7 Days in Chicago🏙</h1>
-      <h2>Welcome to Chicago, Natalia! Check out this calendar to get to know the city and see all the sights during your stay.</h2>
+      <h3>Welcome to Chicago, Natalia! Check out this calendar to get to know the city and see all the sights during your stay.</h3>
       <Calendar />
     </div>
   )
