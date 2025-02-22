50c3248a9f9c46d54d929ca4b0dcfaa9259fd2e2
nebjamin <jonb1@newpaltz.edu>
2025-02-22 00:39:09 -0500

    Lab 1 Checkpoint 3 Modified

--- timetabled/src/components/Calendar.jsx
+++ timetabled/src/components/Calendar.jsx
@@ -17,106 +17,24 @@ const Calendar = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td className="time">8 am</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td className="time">9 am</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td className="time">10 am</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td className="time">11 am</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td className="time">12 pm</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td className="time">1 pm</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td className="time">2 pm</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td className="time">3 pm</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td className="time">4 pm</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td className="time">5 pm</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
+                {Array.from({ length: 10 }).map((_, index) => {
+                    const hour = index + 8;
+                    const period = hour < 12 ? "AM" : "PM";
+                    const displayHour = hour > 12 ? hour - 12 : hour; // Convert 13-17 to 1-5
+
+                    return (
+                        <tr key={index}>
+                            <td className="time">{displayHour} {period}</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    );
+                })}
                 </tbody>
             </table>
         </div>
