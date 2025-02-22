38950ba031aeb6a5f0da44bc23d699e8f69890eb
nebjamin <jonb1@newpaltz.edu>
2025-02-22 00:49:09 -0500

    Lab 1 Checkpoint 4 and 5

--- timetabled/src/components/Calendar.jsx
+++ timetabled/src/components/Calendar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Event from './Event';
 
 const Calendar = () => {
     return (
@@ -17,24 +18,109 @@ const Calendar = () => {
                     </tr>
                 </thead>
                 <tbody>
-                {Array.from({ length: 10 }).map((_, index) => {
-                    const hour = index + 8;
-                    const period = hour < 12 ? "AM" : "PM";
-                    const displayHour = hour > 12 ? hour - 12 : hour; // Convert 13-17 to 1-5
-
-                    return (
-                        <tr key={index}>
-                            <td className="time">{displayHour} {period}</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                    );
-                })}
+                    <tr>
+                        <td className="time">8 am</td>
+                        <Event
+                            event='Fancy Dinner 🎩'
+                            color='green'
+                        />
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">9 am</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">10 am</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">11 am</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">12 pm</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">1 pm</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">2 pm</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">3 pm</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">4 pm</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td className="time">5 pm</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
                 </tbody>
             </table>
         </div>
