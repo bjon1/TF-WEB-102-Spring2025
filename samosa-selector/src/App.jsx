86585e784f461165cea84f25c816f39c2fdafce4
nebjamin <jonb1@newpaltz.edu>
2025-02-23 21:05:53 -0500

    Lab 2 Step 4

--- samosa-selector/src/App.jsx
+++ samosa-selector/src/App.jsx
@@ -7,6 +7,24 @@ const App = () => {
   const [multiplier, setMultiplier] = useState(1);
 
   const updateCount = () => setCount(count + multiplier);
+  const buyDoubleStuffed = () => {
+    if (count >= 10) {
+      setMultiplier(multiplier * 2);
+      setCount(count - 10);
+    }
+  }
+  const buyPartyPack = () => {
+    if (count >= 100) {
+      setMultiplier(multiplier * 5);
+      setCount(count - 100);
+    }
+  }
+  const buyFullFeast = () => {
+    if (count >= 1000) {
+      setMultiplier( multiplier * 10);
+      setCount(count - 1000);
+    }
+  }
 
   return (
     <div className="App">
@@ -24,17 +42,17 @@ const App = () => {
         <div className="upgrade">
           <h3>Double Stuffed 👯‍♀️</h3>
           <p>2x per click</p>
-          <button onClick={() => setMultiplier(2)}>Buy for 10 Samosas</button>
+          <button onClick={buyDoubleStuffed}>Buy for 10 Samosas</button>
         </div>
         <div className="upgrade">
           <h3>Party Pack 🎉</h3>
           <p>5x per click</p>
-          <button onClick={() => setMultiplier(2)}>Buy for 100 Samosas</button>
+          <button onClick={buyPartyPack}>Buy for 100 Samosas</button>
         </div>
         <div className="upgrade">
           <h3>Full Feast 👩🏽‍🍳</h3>
           <p>10x per click</p>
-          <button onClick={() => setMultiplier(2)}>Buy for 1000 Samosas</button>
+          <button onClick={buyFullFeast}>Buy for 1000 Samosas</button>
         </div>
       </div>
     </div>
