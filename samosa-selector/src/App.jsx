6ca162e3973f84028c3db0895262e17524d7410e
nebjamin <jonb1@newpaltz.edu>
2025-02-23 20:51:43 -0500

    Lab 2 Step 2

--- samosa-selector/src/App.jsx
+++ samosa-selector/src/App.jsx
@@ -1,15 +1,22 @@
 import './App.css';
+import { useState } from 'react';
 
 const App = () => {
+
+  const [count, setCount] = useState(0);
+  const [multiplier, setMultiplier] = useState(1);
+
+  const updateCount = () => setCount(count + multiplier);
 
   return (
     <div className="App">
       <div className="header">
         <h1>Samosa Selector</h1>
-        <h2>Count: </h2>
+        <h2>Count: {count}</h2>
         <img 
           src="https://helpful-griffin-0a0ba5.netlify.app/samosa.png" 
           className="samosa" 
+          onClick={updateCount}
         />
       </div>
     </div>
