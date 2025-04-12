360df538a149123b91e5f38ce929572db86bfcbe
nebjamin <jonb1@newpaltz.edu>
2025-04-12 12:54:46 -0400

    Lab 8 Step 7

--- bet/src/pages/EditPost.js
+++ bet/src/pages/EditPost.js
@@ -19,6 +19,17 @@ const EditPost = ({data}) => {
         window.location = "/";
     }
 
+    const deletePost = async (event) => {
+        event.preventDefault();
+      
+        await supabase
+          .from('Posts')
+          .delete()
+          .eq('id', id);
+      
+        window.location = "/";
+    }
+
     const handleChange = (event) => {
         const {name, value} = event.target;
         setPost( (prev) => {
@@ -45,7 +56,7 @@ const EditPost = ({data}) => {
                 </textarea>
                 <br/>
                 <input type="submit" value="Submit" onClick={updatePost}/>
-                <button className="deleteButton">Delete</button>
+                <button className="deleteButton" onClick={deletePost}>Delete</button>
             </form>
         </div>
     )
